e381158a4288021bc664ff04128ec3a43cb1285b
Chirag Gupta <chirag.gupta2992@gmail.com>
2024-02-25 18:21:49 -0500

    Add project files.

---
+++ stackoverflow_recommendation_system/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using stackoverflow_recommendation_system.Models;
+using System.Diagnostics;
+
+namespace stackoverflow_recommendation_system.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ stackoverflow_recommendation_system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace stackoverflow_recommendation_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
