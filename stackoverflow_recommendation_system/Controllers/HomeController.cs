78a1e3e7e51b5a02ac1322633eb4ef531d4ae9a7
siddharth9805 <siddharth.u@somaiya.edu>
2024-02-26 16:06:36 -0500

    updated code

--- stackoverflow_recommendation_system/Controllers/HomeController.cs
+++ stackoverflow_recommendation_system/Controllers/HomeController.cs
@@ -1,20 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
 using stackoverflow_recommendation_system.Models;
 using System.Diagnostics;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using System.Data.SqlClient;
 
 namespace stackoverflow_recommendation_system.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IConfiguration config)
         {
+            this.configuration = config;
             _logger = logger;
         }
-
-        public IActionResult Index()
+        
+        [HttpPost]
+        public ActionResult SubmitForm(search_prompt search)
         {
+            // You can now access the form data through the `person` object.
+            // For example:
+            ViewBag.Message = $"Search: {search.searchQuery}";
+            return View("Index");
+        }
+        
+        [HttpGet]
+        public ActionResult FetchData(int pageNumber)
+        {
+            string? connectionString = configuration.GetConnectionString("DefaultConnection");
+            if (connectionString == null)
+            {
+                throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            }
+
+            int pageSize = 10; // Set the number of records per page
+            using (var db = new SqlConnection(connectionString))
+            {
+                var offset = pageSize * (pageNumber - 1);
+                var sql = @"SELECT * FROM Badges ORDER BY Id OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY;";
+                var data = db.Query<dynamic>(sql, new { Offset = offset, PageSize = pageSize }).ToList();
+                return Json(data);
+            }
+        }
+        
+        
+        public IActionResult Index()
+        {   
             return View();
         }
 
--- stackoverflow_recommendation_system/Program.cs
+++ stackoverflow_recommendation_system/Program.cs
@@ -1,7 +1,14 @@
+using stackoverflow_recommendation_system.Controllers;
+using stackoverflow_recommendation_system.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSignalR();
+
+builder.Services.AddHostedService<DatabasePollingService>();
 
 var app = builder.Build();
 
@@ -23,5 +30,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<NotificationHub>("/notificationHub");
 
 app.Run();
